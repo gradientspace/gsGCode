52f748134e354af05e24db58be6b1db2f3046c0c
Ryan Schmidt <rms@rms80.com>
2018-03-04 21:56:31 -0500

    changed defaults

--- settings/AdditiveSettings.cs
+++ settings/AdditiveSettings.cs
@@ -237,8 +237,8 @@ namespace gs
         /*
          * Support settings
          */
-        public bool EnableSupport = false;
-        public double SupportSpacingStepX = 3.0;         // usage depends on support technique?           
+        public bool EnableSupport = true;
+        public double SupportSpacingStepX = 5.0;         // usage depends on support technique?           
         public double SupportVolumeScale = 0.9;          // multiplier on extrusion volume
         public bool EnableSupportShell = true;           // should we print a shell around support areas
         public double SupportGapInLayerX = 1.25f;        // multiple of Machine.NozzleDiam that we leave between support and print
